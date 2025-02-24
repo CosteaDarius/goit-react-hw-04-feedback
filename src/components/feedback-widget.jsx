c29ff7ad877a329238e40b2fbd98f484a5d7dabc
CosteaDarius <dariuscostea068@gmail.com>
2025-02-24 22:10:43 +0200

    c

---
+++ src/components/feedback-widget.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import FeedbackOptions from "./feedback-options";
+import Statistics from "./statistics";
+import Section from "./section";
+import Notification from "./notification";
+
+const FeedbackWidget = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((feedback.good / total) * 100) : 0;
+  };
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={["good", "neutral", "bad"]} onLeaveFeedback={handleFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default FeedbackWidget;
